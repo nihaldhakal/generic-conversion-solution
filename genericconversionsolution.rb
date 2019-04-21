e931066626da421a64759454a5820446eef43d19
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-04-21 21:53:35 +0545

    Kept only one while

--- genericconversionsolution.rb
+++ genericconversionsolution.rb
@@ -1,22 +1,19 @@
 def convert_number(number_to_be_converted,from,to)
   arr=Array.new
   i=0
-  if from == "decimal" && to == "binary"
-    while(number_to_be_converted!=0)
+  while(number_to_be_converted!=0)
+    if from == "decimal" && to == "binary"
       arr[i]= number_to_be_converted%2
       number_to_be_converted=number_to_be_converted/2
       i+=1
-    end
-    puts arr.join.reverse!
-  elsif from == "decimal" && to =="octal"
-    while(number_to_be_converted!=0)
+    elsif from == "decimal" && to =="octal"
       arr[i]= number_to_be_converted%8
       number_to_be_converted=number_to_be_converted/8
       i+=1
+    else
+      puts"Error"
     end
-    puts arr.join.reverse!
-  else
-    puts"Error"
   end
+  puts arr.join.reverse!
 end
 convert_number(10, "decimal","binary")
