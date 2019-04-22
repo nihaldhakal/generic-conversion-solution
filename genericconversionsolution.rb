4c5e65a4bb79c143a15bfcf5abb1f86b7c009ce7
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-04-22 09:33:37 +0545

    Created a engine

--- genericconversionsolution.rb
+++ genericconversionsolution.rb
@@ -1,19 +1,18 @@
 def convert_number(number_to_be_converted,from,to)
   arr=Array.new
   i=0
+  divisor = if from == "decimal" && to == "binary"
+              2
+            elsif from == "decimal" && to =="octal"
+              8
+            end
   while(number_to_be_converted!=0)
-    if from == "decimal" && to == "binary"
-      arr[i]= number_to_be_converted%2
-      number_to_be_converted=number_to_be_converted/2
-      i+=1
-    elsif from == "decimal" && to =="octal"
-      arr[i]= number_to_be_converted%8
-      number_to_be_converted=number_to_be_converted/8
-      i+=1
-    else
-      puts"Error"
-    end
+    arr[i]= number_to_be_converted%divisor
+    number_to_be_converted=number_to_be_converted/divisor
+    i+=1
   end
+
   puts arr.join.reverse!
 end
+convert_number(10, "decimal","octal")
 convert_number(10, "decimal","binary")
